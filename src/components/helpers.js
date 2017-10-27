f6b0667ccefec9e6da8e88841358033ab420793a
Rodolfo Stangherlin <rodolfo.3@gmail.com>
2017-10-27 00:18:37 -0200

    Use "loading" while data is not ready for HOC withActions

--- src/components/helpers.js
+++ src/components/helpers.js
@@ -2,17 +2,37 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 
+const Loading = () => <strong>Loading...</strong>;
+
+
 export const withActions = actions => OriginalComponent => {
   class WithActioncComponent extends Component {
-    componentDidMount() {
-      const { dispatch } = this.props;
+    state = {
+      loaded: false,
+    }
 
-      Promise.all(actions.map(act => dispatch(act)))
-      .then(r => console.log('loaded!', r))
+    componentDidMount() {
+setTimeout(() => {
+      if (actions.length) {
+        const { dispatch } = this.props;
+        Promise.all(actions.map(act => dispatch(act)))
+          .then(() => {
+            console.log(this.props);
+            this.setState({ loaded: true });
+          })
+      } else {
+          this.setState({ loaded: true });
+      }
+}, 300);
     }
 
     render() {
-      return <OriginalComponent {...this.props} />
+      if (this.state.loaded) {
+console.log('component');
+        return <OriginalComponent {...this.props} />
+      }
+console.log('loading');
+      return <Loading />;
     }
   }
 
