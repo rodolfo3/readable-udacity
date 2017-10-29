f4cff6c62b0223bb920dbe3b828ac5daa7d1e8f0
Rodolfo Stangherlin <rodolfo.3@gmail.com>
2017-10-29 18:15:44 -0200

    Setup react-router

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
 import './App.css';
 import CategoryList from './CategoryList';
+import GenericNotFound from './GenericNotFound';
 
 
 class App extends Component {
   render() {
     return (
-      <CategoryList />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={CategoryList} />
+          <Route exact path='*' component={GenericNotFound} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/GenericNotFound.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+
+export default () => <b>Not Found</b>;
