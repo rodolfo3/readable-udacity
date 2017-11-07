56fedf02743da44c0ada7249ec7ec3f7b2cfc4ec
Rodolfo Stangherlin <rodolfo.3@gmail.com>
2017-11-07 05:59:15 -0200

    Save a new post

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
-import { LOADED } from '../actions/posts';
+import { LOADED, SAVED } from '../actions/posts';
 
 
 const byId = (state = {}, action) => {
 console.log('byId', action, action.type === LOADED);
   switch (action.type) {
     case LOADED:
+    case SAVED:
+      console.log("!!!", action);
       const newPosts = action.posts.reduce((acc, p) => ({...acc, [p.id]: p}), {});
       return ({...state, ...newPosts});
 
@@ -16,6 +18,7 @@ console.log('byId', action, action.type === LOADED);
 
 const byCategory = (state = {}, action) => {
   switch (action.type) {
+    case SAVED:
     case LOADED:
       if (action.categoryPath) {
         const postIds = action.posts.map(p => p.id)
