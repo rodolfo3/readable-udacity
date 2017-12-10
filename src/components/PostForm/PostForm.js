ec8930d7daae73c8774c8b3d33bfdefc971ac072
Rodolfo Stangherlin <rodolfo.3@gmail.com>
2017-12-10 19:51:14 -0200

    Add style to PostForm

--- src/components/PostForm/PostForm.js
+++ src/components/PostForm/PostForm.js
@@ -1,63 +1,40 @@
 import React from 'react';
 import serializeForm from 'form-serialize';
 
+
+import './PostForm.css';
+
+
 const PostForm = ({ categories, post, submitForm }) => {
   const innerSubmitForm = (ev) => {
     submitForm(serializeForm(ev.target, { hash: true }));
     ev.preventDefault();
   }
-  console.log(post);
+
   return (
-    <form onSubmit={innerSubmitForm}>
-      { post.id && <input type="hidden" name="id" value={post.id} /> }
-      <label>
+    <form onSubmit={innerSubmitForm} className="post-form">
+      { post.id && <input className="post-form-field" type="hidden" name="id" value={post.id} /> }
+
+      <label className="post-form-label">
         Title
-        <input type="text" name="title" defaultValue={post.title} required />
+        <input className="post-form-field" type="text" name="title" defaultValue={post.title} required />
       </label>
 
-      <label>
+      <label className="post-form-label">
         Author
-        <input type="text" name="author" defaultValue={post.author} required />
+        <input className="post-form-field" type="text" name="author" defaultValue={post.author} required />
       </label>
 
-      <label>
+      <label className="post-form-label">
         Category
-        <select name="category" defaultValue={post.category}>
+        <select className="post-form-field" name="category" defaultValue={post.category}>
           { categories && categories.map(c => <option key={c.path} value={c.path}>{c.name}</option>) }
         </select>
       </label>
 
-      <label>
+      <label className="post-form-label">
         Post body
-        <textarea name="body" defaultValue={post.body} required></textarea>
-      </label>
-
-      <input type="submit" />
-    </form>
-  );
-
-  return (
-    <form onSubmit={innerSubmitForm}>
-      <label>
-        Title
-        <input type="text" name="title" required />
-      </label>
-
-      <label>
-        Author
-        <input type="text" name="author" required />
-      </label>
-
-      <label>
-        Category
-        <select name="category">
-          { categories && categories.map(c => <option key={c.path} value={c.path}>{c.name}</option>) }
-        </select>
-      </label>
-
-      <label>
-        Post body
-        <textarea name="body" required></textarea>
+        <textarea className="post-form-field" name="body" defaultValue={post.body} required></textarea>
       </label>
 
       <input type="submit" />
